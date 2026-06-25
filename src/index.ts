05d35adfaaa9a89037a6c23bcb097bcdf8981b30
Suyel Haque <slsuyel@gmail.com>
2026-06-25 16:53:54 +0600

    feat: implement Prisma database client with ESM support and tsx execution

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,32 @@
-console.log("Hello, Prisma!");
+import "dotenv/config";
+import { PrismaClient } from "../generated/prisma/client.js";
+import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
+
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
+
+async function main() {
+  //   const result = await prisma.post.create({
+  //     data: {
+  //       title: "Post 3",
+  //       content: "Content 1",
+  //       published: true,
+  //       authorName: "John Doe",
+  //     },
+  //   });
+  //   console.log(result);
+  // const getAllPostFromDb = await prisma.post.findMany();
+  // console.log(getAllPostFromDb);
+  //   const getSInglePost = await prisma.post.findUnique({
+  //     where: {
+  //       id: 1,
+  //     },
+  //   });
+  //   console.log(getSInglePost);
+}
+
+main();
